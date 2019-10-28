441b341626acdd1bc0f65446f218d443ea492c51
AmishAlvi <amish.alvi@gmail.com>
2019-10-28 21:54:25 +0300

    Add files via upload

---
+++ Twitch Controller Vibration Script/Boilerplate/Template_StreamlabsSystem.py
@@ -0,0 +1,124 @@
+#---------------------------
+#   Import Libraries
+#---------------------------
+import os
+import sys
+import json
+import ctypes
+import time
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib")) #point at lib folder for classes / references
+
+import clr
+clr.AddReference("IronPython.SQLite.dll")
+clr.AddReference("IronPython.Modules.dll")
+
+#   Import your Settings class
+from Settings_Module import MySettings
+
+#---------------------------
+#   [Required] Script Information
+#---------------------------
+ScriptName = "Template Script"
+Website = "https://www.streamlabs.com"
+Description = "!test will post a message in chat"
+Creator = "AnkhHeart"
+Version = "1.0.0.0"
+
+#---------------------------
+#   Define Global Variables
+#---------------------------
+global SettingsFile
+SettingsFile = ""
+global ScriptSettings
+ScriptSettings = MySettings()
+
+#---------------------------
+#   [Required] Initialize Data (Only called on load)
+#---------------------------
+# Define necessary structures
+class XINPUT_VIBRATION(ctypes.Structure):
+    _fields_ = [("wLeftMotorSpeed", ctypes.c_ushort),
+                ("wRightMotorSpeed", ctypes.c_ushort)]
+
+xinput = ctypes.windll.xinput1_1  # Load Xinput.dll
+
+# Set up function argument types and return type
+XInputSetState = xinput.XInputSetState
+XInputSetState.argtypes = [ctypes.c_uint, ctypes.POINTER(XINPUT_VIBRATION)]
+XInputSetState.restype = ctypes.c_uint
+
+# You can also create a helper function like this:
+def set_vibration(controller, left_motor, right_motor):
+    vibration = XINPUT_VIBRATION(int(left_motor * 65535), int(right_motor * 65535))
+    XInputSetState(controller, ctypes.byref(vibration))
+
+
+def Init():
+
+    #   Create Settings Directory
+    directory = os.path.join(os.path.dirname(__file__), "Settings")
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    #   Load settings
+    SettingsFile = os.path.join(os.path.dirname(__file__), "Settings\settings.json")
+    ScriptSettings = MySettings(SettingsFile)
+    ScriptSettings.Response = "Overwritten pong! ^_^"
+    return
+
+#---------------------------
+#   [Required] Execute Data / Process messages
+#---------------------------
+def Execute(data):
+    if data.IsChatMessage() and data.GetParam(0).lower() == ScriptSettings.Command and Parent.IsOnUserCooldown(ScriptName,ScriptSettings.Command,data.User):
+        Parent.SendStreamMessage("Time Remaining " + str(Parent.GetUserCooldownDuration(ScriptName,ScriptSettings.Command,data.User)))
+
+    #   Check if the propper command is used, the command is not on cooldown and the user has permission to use the command
+    if data.IsChatMessage() and data.GetParam(0).lower() == ScriptSettings.Command and not Parent.IsOnUserCooldown(ScriptName,ScriptSettings.Command,data.User) and Parent.HasPermission(data.User,ScriptSettings.Permission,ScriptSettings.Info):
+        Parent.BroadcastWsEvent("EVENT_MINE","{'show':false}")
+        Parent.SendStreamMessage(ScriptSettings.Response)    # Send your message to chat
+        Parent.AddUserCooldown(ScriptName,ScriptSettings.Command,data.User,ScriptSettings.Cooldown)  # Put the command on cooldown
+        set_vibration(0, 0.5, 0.5)
+        t = 5
+        time.sleep(5)
+        set_vibration(0, 0 , 0)
+
+    
+    return
+
+#---------------------------
+#   [Required] Tick method (Gets called during every iteration even when there is no incoming data)
+#---------------------------
+def Tick():
+    return
+
+#---------------------------
+#   [Optional] Parse method (Allows you to create your own custom $parameters) 
+#---------------------------
+def Parse(parseString, userid, username, targetid, targetname, message):
+    
+    if "$myparameter" in parseString:
+        return parseString.replace("$myparameter","I am a cat!")
+    
+    return parseString
+
+#---------------------------
+#   [Optional] Reload Settings (Called when a user clicks the Save Settings button in the Chatbot UI)
+#---------------------------
+def ReloadSettings(jsonData):
+    # Execute json reloading here
+    ScriptSettings.__dict__ = json.loads(jsonData)
+    ScriptSettings.Save(SettingsFile)
+    return
+
+#---------------------------
+#   [Optional] Unload (Called when a user reloads their scripts or closes the bot / cleanup stuff)
+#---------------------------
+def Unload():
+    return
+
+#---------------------------
+#   [Optional] ScriptToggled (Notifies you when a user disables your script or enables it)
+#---------------------------
+def ScriptToggled(state):
+    return
